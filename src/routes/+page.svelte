f4aae1bd34307a9b5cc1e0f959101f4fd1707c82
Pedro Falco <fpfalco@gmail.com>
2024-04-12 14:28:49 -0300

    first version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,93 +2,112 @@
 	import { onMount } from "svelte";
     import "/src/style.css";
     import * as THREE from 'three';
-    import vertexShader from '$lib/glsl/vertex.glsl';
-    import fragmentShader from '$lib/glsl/fragment.glsl';
-    import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-    
+    import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
+    import GUI from 'lil-gui'; 
 
     let canvas;
-    let bumpMap;
+    let controls;
+    let debug, active;
+    let map, bmap, dmap;
 
     onMount(() => {
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        active = window.location.hash === '#debug';
 
-        // create setting with sizes
+        if (active) {
+            debug = new GUI();
+
+            // add map a dropdown list
+            debug.add({ map: 'map-srchrs-v2.png' }, 'map', ['map-srchrs-v0.png', 'map-srchrs-v1.png','map-srchrs-v2.png', 'map-srchrs-v3.png', 'map-srchrs-v4.png', 'map-srchrs-v5.png', 'map-srchrs-v6.png']).onChange((value) => {
+                console.log(value);
+                map = new THREE.TextureLoader().load(`/${value}`);
+                bmap = new THREE.TextureLoader().load(`/${value}`);
+                dmap = new THREE.TextureLoader().load(`/${value}`);
+                plane.material.map = map;
+                plane.material.needsUpdate = true;
+            });
+
+        }
+
         const sizes = {
             width: window.innerWidth,
             height: window.innerHeight
         };
 
-        let map = new THREE.TextureLoader().load("/map-srchrs-v2.png");
-        let bmap = new THREE.TextureLoader().load("/map-srchrs-v2.png");
-        let dmap = new THREE.TextureLoader().load("/map-srchrs-v2.png");
-                    
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer({ 
+            canvas,
+            antialias: true,
+        });
+        renderer.physicallyCorrectLights = true;
+        renderer.outputColorSpace = THREE.SRGBColorSpace;
+        renderer.toneMapping = THREE.CineonToneMapping;
+        renderer.toneMappingExposure = 1.75;
+        renderer.shadowMap.enabled = true;
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        renderer.setSize(sizes.width, sizes.height);
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-        console.log(bmap)
-
+        map = new THREE.TextureLoader().load("/map-srchrs-v2.png");
+        bmap = new THREE.TextureLoader().load("/map-srchrs-v2.png");
+        dmap = new THREE.TextureLoader().load("/map-srchrs-v2.png");
 
         // Create plane geometry
-        const geometry = new THREE.PlaneGeometry(sizes.width, sizes.height, 1080, 1080);
-        // const geometry = new THREE.PlaneGeometry(sizes.width, sizes.height);
+        const geometry = new THREE.PlaneGeometry(sizes.width, sizes.width, 500, 500);
 
-
-        // const material = new THREE.ShaderMaterial({
-        //     vertexShader: vertexShader,
-        //     fragmentShader: fragmentShader
-        // });
-
-        const material = new THREE.MeshStandardMaterial({
-            // color: 0xff0000,
+        const material = new THREE.MeshPhongMaterial({
             bumpMap: bmap,
-            bumpScale: 10.,
+            bumpScale: 2.,
             map: map,
             displacementMap: dmap,
-            displacementScale: 5.0,
+            displacementScale: 10.0,
 
          })
 
-         camera.position.x = 0;
-        //  camera.position.y = -120;
-         camera.position.z = 170;
+        camera.position.y = 200;
+        camera.rotation.x = -Math.PI / 4;
 
         //import orbitControls
-        const controls = new OrbitControls( camera, renderer.domElement );
+        controls = new FirstPersonControls( camera, renderer.domElement );
+        // controls.autoForward = true;
+        // controls.movementSpeed = 150;
+        controls.lookSpeed = .0025;        
         
-        controls.minPolarAngle = Math.PI - (Math.PI * 2/3);
-        controls.maxPolarAngle = Math.PI * 2/3;
-        controls.minAzimuthAngle = -Math.PI / 8;
-        controls.maxAzimuthAngle = Math.PI / 8;
-
-        // Create mesh with geometry and material
         const plane = new THREE.Mesh(geometry, material);
+        plane.rotation.x = -Math.PI / 2;
         scene.add(plane);
 
-
-        // Add ambient light
         const ambientLight = new THREE.AmbientLight(0xffffff, 1.0); // Color, Intensity
         scene.add(ambientLight);
 
-        scene.fog = new THREE.Fog(0x000000, 1, 400); // color, near, far
 
+        scene.fog = new THREE.Fog(0x080500, 1, 700); // color, near, far
 
         const animate = function () {
             requestAnimationFrame(animate);
 
-            // plane.rotation.x += 0.01;
-            plane.rotation.z += 0.001;
-            // plane.rotation.y += 0.00025;
-
+            plane.rotation.z += 0.0005;
+            
+            if (controls) {controls.update(.05)};
             renderer.render(scene, camera);
         };
 
         animate();
+        
+        onresize = () => {
+            sizes.width = window.innerWidth;
+            sizes.height = window.innerHeight;
+
+            camera.aspect = sizes.width / sizes.height;
+            camera.updateProjectionMatrix();
+
+            controls.handleResize();
+            renderer.setSize(sizes.width, sizes.height);
+        };
     });
 </script>
 
 <canvas bind:this={canvas}></canvas>
 
-<h1 class="text-white absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-6xl">SRCHRS</h1>
+<h1 class="text-white absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-6xl select-none">SRCHRS</h1>
