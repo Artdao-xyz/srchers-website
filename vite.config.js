81f8efc1fabf535f72b99e45d7919941f1c6e089
Pedro Falco <fpfalco@gmail.com>
2024-04-15 15:57:48 -0300

    chore: fixed a bug rss and custom shader material related

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl'
 
 export default defineConfig({
+    ssr: {
+		noExternal: [
+			'three-custom-shader-material',
+		]
+	},
 	plugins: [sveltekit(), glsl()]
 });
